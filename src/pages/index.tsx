dfe3bddaf05c817a82b600c86792b35dfb7a4c8b
Gustavo Assuncao <gust.krv@gmail.com>
2021-12-02 17:09:01 -0300

    Estrutura - DONE

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title> Inicio | ig.news</title>
+      </Head>
+
+      <h1>Hello world</h1>
+    </>
+  )
+}
