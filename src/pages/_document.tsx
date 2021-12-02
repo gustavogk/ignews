c819d9fb1d5f8784168fdbc83e70ca67683ce251
Gustavo Assuncao <gust.krv@gmail.com>
2021-12-02 19:20:03 -0300

    Component - HEADER

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,8 @@ export default class myDocumeny extends Document {
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet" />
+
+                <link rel="shortcut icon" href="/favicon.png" type="image/png" />
             </Head>
             <body>
                 <Main />
