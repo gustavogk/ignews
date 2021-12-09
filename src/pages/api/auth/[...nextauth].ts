f678756a4946d05babb29dc40a39d7bcd724bb00
Gustavo Assuncao <gust.krv@gmail.com>
2021-12-09 19:34:12 -0300

    FaunaDB config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from "faunadb";
+
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,4 +19,42 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+
+      const { email } = user
+      
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email }}
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+          )
+       )
+
+        return true
+
+      } catch {
+
+        return false
+      }
+    },
+  }
 })
