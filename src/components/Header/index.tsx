306964b7b498347a87daaca2fa3fe8e7ffa675db
Gustavo Assuncao <gust.krv@gmail.com>
2021-12-30 20:24:09 -0300

    Config CMS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import { ActiveLink } from '../ActiveLink/index';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header(){
+
+    
     return(
+
         <header className={styles.headerContainer}>
              <div className={styles.headerContent}>
                  <img src="/images/logo.svg" alt="ig.news" />
                  <nav>
-                     <a className={styles.active}> Home </a>
-                     <a> Posts </a>
+                     <ActiveLink activeClassName={styles.active}  href="/">
+                        <a> Home </a>
+                     </ActiveLink>
+                     <ActiveLink activeClassName={styles.active} href='/posts' prefetch>
+                        <a> Posts </a>
+                     </ActiveLink>
                  </nav>
 
                  <SignInButton />
